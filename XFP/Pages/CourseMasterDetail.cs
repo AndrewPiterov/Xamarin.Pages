1730656d4ea2e79e6a4c4845df2389a7523ff9bd
Andrey Piterov <pfm@live.ru>
2017-09-14 21:48:44 +0300

    add Page Navigation and Master Detail Page

---
+++ XFP/Pages/CourseMasterDetail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFP.Pages
+{
+    class CourseMasterDetail : MasterDetailPage
+    {
+        public CourseMasterDetail()
+        {
+            var listView = new ListView();
+            listView.ItemsSource = PluralsightCourse.GetCourseList();
+
+            listView.ItemSelected += (s, e) =>
+            {
+                if (e.SelectedItem != null)
+                {
+                    Detail = new CoursePage(e.SelectedItem as PluralsightCourse);
+                }
+            };
+
+            Master = new ContentPage
+            {
+                Title = "Courses",
+                Content = listView
+            };
+
+            Detail = new CoursePage(PluralsightCourse.GetCourseList().First());
+        }
+    }
+}
---
+++ XFP/Pages/CoursePage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFP.Pages
+{
+    class CoursePage : ContentPage
+    {
+        public CoursePage(PluralsightCourse course)
+        {
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    Padding = new Thickness(10, 20, 10, 0);
+                    break;
+                default:
+                    Padding = new Thickness(10, 0);
+                    break;
+            }
+
+            BackgroundColor = Color.Gray;
+
+            this.Title = course.TitleShort;
+
+            var titleLabel = new Label
+            {
+                Text = course.Title,
+                Font = Font.SystemFontOfSize(NamedSize.Large)
+            };
+
+            var authorLabel = new Label
+            {
+                Text = course.Author,
+                Font = Font.SystemFontOfSize(NamedSize.Small)
+            };
+
+            var descriptionLabel = new Label
+            {
+                Text = course.Description,
+                Font = Font.SystemFontOfSize(NamedSize.Medium)
+            };
+
+            Content = new ScrollView
+            {
+                Content = new StackLayout
+                {
+                    Spacing = 10,
+                    Children = { titleLabel, authorLabel, descriptionLabel }
+                }
+            };
+        }
+    }
+}
